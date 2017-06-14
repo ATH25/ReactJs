a4aaa3c8865de03beb97a1b74659bb294cdb0388
ATH25 <aju_thomas@yahoo.com>
2017-06-14 08:44:53 -0400

    Create code7.js

---
+++ code7.js
@@ -0,0 +1,40 @@
+
+
+const Card = (props) => {
+	return(
+  	<div style={{margin:'1em'}}>
+      <img width="75" src={props.avatar_url}/>
+      <div style={{display:'inline-block', marginLeft:10}}>
+        <div style={{fontSize: '1.25em', fontWeight:'bold'}}>
+          {props.name}
+        </div>  
+        <div>{props.company}</div>  
+      </div>
+    </div>
+  );
+};
+
+const CardList = (props) => {
+	return(
+  	<div>
+      <Card name="Paul"
+      avatar_url="https://avatars3.githubusercontent.com/u/1?v=3"
+      company="Facebook"/>
+    </div>
+  );
+};
+
+ReactDOM.render(<CardList />, mountNode);
+
+
+
+//css to include
+#mountNode {
+  color: #000;
+}
+.info {
+  color: red;
+}
+
+
+
